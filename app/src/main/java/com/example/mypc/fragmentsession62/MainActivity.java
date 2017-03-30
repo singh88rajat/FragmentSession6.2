17626c01c34a27e374b3872b03e8c01249749cf6
Mukesh Kumar <singh88rajat@gmail.com>
2017-03-30 23:11:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mypc/fragmentsession62/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mypc.fragmentsession62;
+
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
